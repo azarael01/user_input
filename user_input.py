2a45d6a53e2416e7bb9b75ae78c3ffb98348359b
azarael01 <139633910+azarael01@users.noreply.github.com>
2024-02-22 22:36:10 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# name
+name = input("Enter your name: ")
+
+# age
+age = input("Enter your age: ")
+
+# location
+location = input("Enter your location: ")
+
+# Output
+print(f"Hello {name}, you are  {age} years old and live in {location}.")
